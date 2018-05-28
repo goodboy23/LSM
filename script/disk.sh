fa069f29493075352dc7e93480ee47e5d1b0afb5
goodboy23 <31239628+goodboy23@users.noreply.github.com>
2018-05-28 10:22:43 +0800

    Update disk.sh

--- script/disk.sh
+++ script/disk.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 #磁盘监控组
 
-#查询硬盘i节点
+#查询跟目录使用情况
 disk_gen_space() {
-    local dis_i=`df -Th | grep -w "/" | awk '{print $6}' | awk -F'%' '{print $1}'` #获取的值
+    local dis_gen=`df -Th | grep -w "/" | awk '{print $6}' | awk -F'%' '{print $1}'` #获取的值
     local value=80 #报警阀值
-    local caveat="根目录使用空间超过${value}，当前获取值${dis_i}" #警告话语
+    local caveat="根目录使用空间超过${value}，当前获取值${dis_gen}" #警告话语
     
     #日志
     date +%F/%H/%M/%S  >> $lsm_log
-    echo 根目录使用空间获取值：${dis_i} >> $lsm_log
+    echo 根目录使用空间获取值：${dis_gen} >> $lsm_log
     
-    if [ $dis_i -ge $value ];then
+    if [ $dis_gen -ge $value ];then
         echo $caveat
         echo $caveat >> $lsm_log
     fi
