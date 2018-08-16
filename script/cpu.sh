104decb381c66cf20877fbd340e5b58523aa7d1e
goodboy23 <31239628+goodboy23@users.noreply.github.com>
2018-08-16 14:26:45 +0800

    Update cpu.sh

--- script/cpu.sh
+++ script/cpu.sh
@@ -7,7 +7,7 @@ cpu_five_load () {
     local cpu_num=`grep 'model name' /proc/cpuinfo |wc -l` #统计cpu核数
     local load_average=`uptime | awk -F ":" '{print $NF}'|awk -F "," '{print $3}'` #load average取值。
     local key=`bc <<< ${load_average}*100/${cpu_num} | awk -F'.' '{print $1}'`
-    local value=8000 #报警阀值
+    local value=80 #报警阀值
     local caveat="cpu使用超过${value}，当前值${key}" # 警告话语
 
     data_log cpu_five_load
